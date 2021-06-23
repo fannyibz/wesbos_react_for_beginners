a4a05d8ba1e18e8e6f655f22ed61f4b3f7cc7ec6
fannyibanez <fannyibanez89@gmail.com>
2021-06-23 12:15:14 +0200

    Invetory: form ok

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class AddFishForm extends React.Component {
   render() {
-    return <p>It ok!!!!</p>;
+    return (
+      <form className="fish-edit">
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <select name="status" id="">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea name="desc" placeholder="Desc"></textarea>
+        <input type="image" type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    );
   }
 }
 
