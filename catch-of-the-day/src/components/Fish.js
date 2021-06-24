fd6c4a094eb55fca9b1b71299d80539818abba92
fannyibanez <fannyibanez89@gmail.com>
2021-06-24 15:14:20 +0200

    button: disabled if not available

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,16 +3,17 @@ import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
   render() {
-    const { name, image, price, desc, availability } = this.props.details;
+    const { name, image, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
-      <li className="menu-fish">
+      <li className='menu-fish'>
         <img src={image} alt={name} />
-        <h3 className="fish-name">
+        <h3 className='fish-name'>
           {name}
-          <span className="price">{formatPrice(price)}</span>
+          <span className='price'>{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to cart</button>
+        <button disabled={!isAvailable}>Add to cart</button>
       </li>
     );
   }
