6e90a61048413946926ee44aa3646cd45e0cee92
Fanny Ibanez <fannyibanez89@gmail.com>
2021-06-29 11:48:30 +0200

    displaying order state ok (#8)

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,8 +5,16 @@ class Order extends React.Component {
   renderOrder = (key) => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
+    }
     return (
-      <li>
+      <li key={key}>
         {count} lbs {fish.name}
         total {formatPrice(fish.price * count)}
       </li>
@@ -26,8 +34,11 @@ class Order extends React.Component {
     return (
       <div className='order-wrap'>
         <h2>Order</h2>
-        <ul>{orderIds.map(this.renderOrder)}</ul>
-        Total: {formatPrice(total)}
+        <ul className='order'>{orderIds.map(this.renderOrder)}</ul>
+        <div className='total'>
+          Total:
+          <strong> {formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
